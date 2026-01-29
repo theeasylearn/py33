015726362830cb7e27caed62c73f869519dc4772
theeasylearn <theeasylearn@gmail.com>
2026-01-29 13:58:37 +0530

    while loop

---
+++ 23_while_1.py
@@ -0,0 +1,13 @@
+# write a program to print following series 
+# 1 3 5 7 9 11 ..... 100
+num  = 1
+# arithmetic expression (addition subtraction multiplication division)
+
+while num<100:
+    print(num,end=' ') #next print statement will be printed on same line   
+    num = num + 2 #3
+
+#empty line to display next line on new line 
+print('') 
+print("Good bye.")
+
---
+++ 24_while_2.py
@@ -0,0 +1,9 @@
+# write a program to print following series 
+# 1 4 9 16 25 ..... 1000
+# 1 2 3 4  5 
+num = 1
+print(num,end=' ') #1
+
+while num<=30:
+    num = num + 1 #2
+    print(num*num,end=' ')
